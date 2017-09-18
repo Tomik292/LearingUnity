1fbaa3233e2f7df379194646944d4ac4a5d4c766
Tomáš Houfek <tomashoufek4@seznam.cz>
2017-09-18 22:49:57 +0200

    Basic path finding and player follow

---
+++ EnemynavMesh.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Diagnostics;
+
+public class EnemynavMesh : MonoBehaviour {
+
+    public Transform enemy;
+    public NavMeshAgent navMesh;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        navMesh = GetComponent<NavMeshAgent>();
+        enemy = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        navMesh.destination = enemy.position;
+	}
+}
